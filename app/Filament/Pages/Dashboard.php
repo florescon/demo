ab9c15140f6f956abab17ea4656be7c986c630f9
florescon <florescontrer@gmail.com>
2024-09-05 22:05:23 -0600

    sep0524

--- app/Filament/Pages/Dashboard.php
+++ app/Filament/Pages/Dashboard.php
@@ -9,28 +9,6 @@ use Filament\Forms\Form;
 use Filament\Forms\Get;
 use Filament\Pages\Dashboard as BaseDashboard;
 
-class Dashboard extends BaseDashboard
+class Dashboard extends \Filament\Pages\Dashboard
 {
-    use BaseDashboard\Concerns\HasFiltersForm;
-
-    public function filtersForm(Form $form): Form
-    {
-        return $form
-            ->schema([
-                Section::make()
-                    ->schema([
-                        Select::make('businessCustomersOnly')
-                            ->label(__('Business customers only'))
-                            ->boolean(),
-                        DatePicker::make('startDate')
-                            ->label(__('Start date'))
-                            ->maxDate(fn (Get $get) => $get('endDate') ?: now()),
-                        DatePicker::make('endDate')
-                            ->label(__('End date'))
-                            ->minDate(fn (Get $get) => $get('startDate') ?: now())
-                            ->maxDate(now()),
-                    ])
-                    ->columns(3),
-            ]);
-    }
 }
